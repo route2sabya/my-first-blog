89e51f2361fbdeb1494b95fe5667df25ebfa7b6d
sabyasachi <sabyasachi_purkayastha@yahoo.co.in>
2017-01-10 15:18:57 +0530

    modified html by using templates to display posts from database

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 from django.utils import timezone
+import datetime
 
 class Post(models.Model):
 	author = models.ForeignKey('auth.User')
@@ -18,5 +19,3 @@ class Post(models.Model):
 
 	def __str__(self):
 		return self.title
-
-		
